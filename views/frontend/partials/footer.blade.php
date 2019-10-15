5be10f28b5127036daffd3e3f4613e436e10caac
Sixl-Daniel <sixl.daniel@googlemail.com>
2019-10-15 13:03:04 +0200

    Add footer mail link to FE, minor styling front page

--- views/frontend/partials/footer.blade.php
+++ views/frontend/partials/footer.blade.php
@@ -5,6 +5,9 @@
                 <a class="nav-item nav-link" href="{{ url('/') }}"><b>VSPOT</b> Digital Signage Solution</a>
             </div>
             <div class="navbar-nav">
+                <a class="nav-item nav-link email" href="mailto:{{ env('APP_MAIL') }}">{{ env('APP_MAIL') }}</a>
+            </div>
+            <div class="navbar-nav">
                 <a class="nav-item nav-link copyright" href="https://sixl.org" target="_blank">&copy;@php echo date('Y') @endphp Daniel Sixl</a>
             </div>
         </div>
