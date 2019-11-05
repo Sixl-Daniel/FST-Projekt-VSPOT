05c83181980abb924e4c35f2b8d5b449020ccd88
Sixl-Daniel <sixl.daniel@googlemail.com>
2019-11-05 16:44:54 +0100

    Shorter API-Token for new registered users

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -72,7 +72,7 @@ class RegisterController extends Controller
             'last_name' => $data['last_name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
-            'api_token' => Str::random(60),
+            'api_token' => Str::random(32),
         ]);
     }
 }
