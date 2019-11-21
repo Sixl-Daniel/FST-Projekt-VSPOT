6a99c2aeed618c62d92ad32932d17821c69c1992
Sixl-Daniel <sixl.daniel@googlemail.com>
2019-11-21 23:34:30 +0100

    Remove empty lines

--- views/backend/partials/js/flash-message.blade.php
+++ views/backend/partials/js/flash-message.blade.php
@@ -1,41 +1,34 @@
 <script>
-
     const Flash = Swal.mixin({
         showConfirmButton: false,
         timer: 4000,
         backdrop: 'rgba(34,45,50,0.95)'
     })
-
     @if ($message = Session::get('flash'))
         Flash.fire('{{ $message }}');
     @endif
-
     @if ($message = Session::get('flash-info'))
         Flash.fire({
             title: '{{ $message }}',
             type: 'info'
         });
     @endif
-
     @if ($message = Session::get('flash-success'))
         Flash.fire({
             title: '{{ $message }}',
             type: 'success'
         });
     @endif
-
     @if ($message = Session::get('flash-warning'))
         Flash.fire({
             title: '{{ $message }}',
             type: 'warning'
         });
     @endif
-
     @if ($message = Session::get('flash-error'))
         Flash.fire({
             title: '{{ $message }}',
             type: 'error'
         });
     @endif
-
 </script>
