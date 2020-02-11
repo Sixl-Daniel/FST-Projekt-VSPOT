61ca2bea5918b008aded9c11dc54a77875f693b5
Sixl-Daniel <sixl.daniel@googlemail.com>
2020-02-11 11:53:39 +0100

    Add meta for Google: notranslate

--- views/web/master.blade.php
+++ views/web/master.blade.php
@@ -4,6 +4,7 @@
 <meta charset="utf-8">
 <title>VSPOT - Digital Signage Solution</title>
 <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
+<meta name="google" content="notranslate">
 <meta name="author" content="Daniel Sixl">
 @yield('top_info')
 @stack('top_stylesheets_stack')
