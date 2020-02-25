e55a8b3f7290730fc8241bb413261280e1f58330
Sixl-Daniel <sixl.daniel@googlemail.com>
2020-02-25 10:50:58 +0100

    Remove purgecss, update packages

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,30 +1,18 @@
 const mix = require('laravel-mix');
 const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin');
-require('laravel-mix-purgecss');
-
-/* purge options */
-
-$purgeOptions = {
-    folders: ['views','app','resources'],
-    whitelistPatterns: [/^view-/],
-    whitelist: ['vspot', 'skin-black'],
-};
 
 /* frontend build */
 
 mix.js('resources/js/app-frontend.js', 'public/js')
-   .sass('resources/sass/frontend/app-frontend.scss', 'public/css')
-   .purgeCss($purgeOptions);
+   .sass('resources/sass/frontend/app-frontend.scss', 'public/css');
 
 /* backend build */
 
-mix.sass('resources/sass/backend/app-backend.scss', 'public/css')
-   .purgeCss($purgeOptions);
+mix.sass('resources/sass/backend/app-backend.scss', 'public/css');
 
 /* web-access build */
 
-mix.sass('resources/sass/web-access/web-access.scss', 'public/css')
-   .purgeCss($purgeOptions);
+mix.sass('resources/sass/web-access/web-access.scss', 'public/css');
 
 /* service worker */
 
